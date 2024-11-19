b7d727022fc5d8e4b2b24f2217b33d166e46ee57
Tederr <jp3351207@gmail.com>
2024-11-19 08:50:36 -0300

    creating fake data

--- src/app/dados/dadosFake.ts
+++ src/app/dados/dadosFake.ts
@@ -14,5 +14,33 @@ export const dadosFake = [
     "descricao": "Uma S10 indo a 400km por hora, atola em um riacho em carapicuipa, com o motorista comendo um sanduiche e o passageiro comendo um bolo de chocolate.",
     "noticia":
     "Carapicuíba, uma cidade normalmente conhecida por seu ritmo tranquilo e ruas bem asfaltadas, se viu abalada por um evento inusitado. Naquela tarde ensolarada, um veículo S10, impulsionado por um motor aparentemente alucinógeno, rompeu a barreira dos 400 km/h, transformando a pacata via em uma pista de corrida improvável. \n Ao volante, um sujeito completamente alheio à velocidade vertiginosa que atingia, concentrava-se em saborear um sanduíche caprichado. Ao seu lado, um passageiro igualmente despreocupado com a situação, deliciava-se com um bolo de chocolate. A combinação da velocidade insana com a tranquilidade dos ocupantes era surreal. A aventura, no entanto, estava longe de terminar. Em meio à velocidade desenfreada, a S10 encontrou um obstáculo inesperado: um pequeno riacho que cortava a estrada. A água, antes tranquila, foi violentamente cortada pela picape, que, impulsionada pela inércia, mergulhou de frente. O impacto foi brutal. A S10, agora transformada em um submarino terrestre, afundou no riacho, levando consigo os dois ocupantes ainda atônitos com a reviravolta dos acontecimentos. O sanduíche e o bolo de chocolate, antes símbolos de um passeio tranquilo, agora jaziam submersos, testemunhas mudas de uma jornada épica. A notícia se espalhou rapidamente, transformando a pequena Carapicuíba em uma cidade das atenções. A polícia, os bombeiros e uma multidão curiosa cercaram o local do acidente, tentando decifrar o mistério por trás daquela velocidade absurda e do desfecho inesperado. A S10 foi retirada do riacho, mas a pergunta que ficou no ar era: como uma picape comum pôde atingir tal velocidade? E mais importante, como os ocupantes conseguiram sair ilesos de tamanha aventura? As teorias mais absurdas ganharam força, desde a existência de um motor experimental até a interferência de forças extraterrestres. A história da S10 voadora, como ficou conhecida, se tornou uma lenda local, um lembrete de que até mesmo em uma cidade tranquila, o inesperado pode acontecer. E quem diria que um sanduíche e um bolo de chocolate poderiam ser os protagonistas de uma história tão épica?",
+  },
+  {
+    "id": "3",
+    "fotoCapa": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWDDei2GPBm5qlIlZEPlyH6dfXKkcMsZhPbg&s",
+    "titulo": "Nova versão do onix",
+    "descricao": "A Volkswagen Onix tem uma nova versão, com um design mais moderno e com mais espaço para o passageiro.",
+    "noticia":"A Chevrolet acaba de revelar a nova geração do Onix, um dos carros mais populares do Brasil. Com um design mais moderno e esportivo, o modelo apresenta um interior mais sofisticado e tecnológico, com destaque para a central multimídia e o painel digital. Sob o capô, novas opções de motorização prometem mais desempenho e eficiência. Além disso, o Onix 2025 chega equipado com uma série de recursos de segurança e assistentes de condução, elevando o patamar do segmento e garantindo uma experiência de direção ainda mais completa e prazerosa."
+  },
+  {
+    "id": "4",
+    "fotoCapa": "https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_640.jpg",
+    "titulo": "Bugatti eletrica? Saiba mais",
+    "descricao": "Bugatti eletrica? Saiba mais",
+    "noticia":"A Bugatti, sinônimo de luxo, performance e tradição, está prestes a revolucionar o mundo dos supercarros mais uma vez. A marca francesa, conhecida por seus motores a combustão de alta potência, está desenvolvendo um modelo totalmente elétrico, prometendo uma nova era de desempenho e sustentabilidade."
+  },
+  {
+    "id": "5",
+    "fotoCapa": "https://cdn.pixabay.com/photo/2019/07/07/14/03/fiat-500-4322521_640.jpg",
+    "titulo": "Qual e o melhor carro para viajar?",
+    "descricao": "Qual e o melhor carro para viajar?",
+    "noticia":"A escolha do carro ideal para uma viagem envolve diversos fatores, como distância a ser percorrida, número de passageiros, tipo de estrada e orçamento. Sedãs médios oferecem conforto e economia, enquanto SUVs compactos são versáteis e ideais para famílias. Minivans acomodam grandes grupos, e station wagons combinam espaço e elegância. Ao escolher, considere o conforto dos assentos, a eficiência do ar-condicionado, a capacidade do porta-malas e os sistemas de segurança. Faça um test drive para ter certeza de que o carro escolhido atende às suas necessidades e preferências. Lembre-se que a segurança é fundamental, portanto, priorize veículos com equipamentos como airbags, freios ABS e controle de estabilidade."
+  },
+  {
+    "id": "6",
+    "fotoCapa": "https://cdn.pixabay.com/photo/2016/09/11/10/02/renault-juvaquatre-1661009_640.jpg",
+    "titulo": "Saiba como se torna placa preta",
+    "descricao": "Saiba como se torna placa preta",
+    "noticia": "A placa preta é um símbolo de status e valor histórico para os veículos antigos. Mas afinal, o que é preciso para que seu carro possa ostentar essa placa tão cobiçada? Para ter direito à placa preta, seu veículo precisa ter, no mínimo, 30 anos de fabricação e estar em excelente estado de conservação, com pelo menos 80% de suas peças originais. Além disso, é necessário se associar a um clube de automóveis antigos credenciado pelo Detran e realizar uma vistoria minuciosa no veículo. Essa vistoria avalia a originalidade das peças, a conservação da pintura, a mecânica e a elétrica. Após a aprovação, o veículo recebe a placa preta, que além de valorizar o carro, oferece benefícios como isenção de IPVA em alguns estados e a possibilidade de participar de eventos exclusivos para veículos antigos."
   }
 ]
